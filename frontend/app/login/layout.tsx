f4f9478477d8b81387a5a1df7fc8908aea03ad15
Gabriel Seet <gabeseet@gmail.com>
2024-01-20 20:16:01 +0800

    remove old frontend and some frontend changes

--- frontend/app/login/layout.tsx
+++ frontend/app/login/layout.tsx
@@ -1,4 +1,4 @@
-export default function AboutLayout({
+export default function LoginLayout({
 	children,
 }: {
 	children: React.ReactNode;
--- frontend/config.jsx
+++ frontend/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    "API_URL": "https://31ce-122-11-177-27.ngrok-free.app",
+    "API_URL": "https://8fc0-122-11-177-28.ngrok-free.app",
 }
 
 export default config;
