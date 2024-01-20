e43f8e37b8575786433288ea568d1abccf389b49
Guo Xiao Yu <108964999+aliensarefake@users.noreply.github.com>
2024-01-20 21:18:38 +0800

    Update fetchNode.js

--- frontend/src/logic-handling/fetchNode.js
+++ frontend/src/logic-handling/fetchNode.js
@@ -1,7 +1,7 @@
 import config from "../config"
 
 export const getNodeTitles = async () => {
-    const res = await fetch(`${config.API_URL}/notes`, {
+    const res = await fetch(`${config.API_URL}/api/notes`, {
         method: 'GET',
         credentials: 'include',
         headers: { 'Content-Type': 'application/json' },
