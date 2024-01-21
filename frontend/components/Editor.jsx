4ac8afcc14a24a5f4860814547eb04308d2110f6
Gabriel Seet <gabeseet@gmail.com>
2024-01-21 09:33:55 +0800

    stykes

--- frontend/components/Editor.jsx
+++ frontend/components/Editor.jsx
@@ -288,7 +288,7 @@ export const Editor = ({ data, setData }) => {
   }
 
   return (
-    <section className="px-0 min-w-7xl flex-grow">
+    <section className="px-0 flex-grow">
       <Modal isOpen={isOpen} placement="top" onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
